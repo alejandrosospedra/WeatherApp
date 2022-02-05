3adf4d197e61ee02f37481d4446cb3a75b206b64
alejandro.sospedra <alejandro.sospedra@gmail.com>
2022-02-05 23:09:44 +0100

    getWeather method

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.weatherapp;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +18,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public String getWeather (String urlPetition) throws IOException{
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlPetition);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        for (String line; (line = reader.readLine()) != null; ) {
+            result.append(line);
+        }
+        return result.toString();
+    }
+
 }
