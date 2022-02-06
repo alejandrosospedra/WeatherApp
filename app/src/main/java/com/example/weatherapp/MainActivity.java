9fa87a5e85ef61e9d19d32a9d141f3b1ea93e267
alejandro.sospedra <alejandro.sospedra@gmail.com>
2022-02-06 21:45:51 +0100

    add get Request file to split the api calls to the activity of the app, also get specific data from the json response from the weather api

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,29 +1,17 @@
 package com.example.weatherapp;
 
-import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.loader.content.AsyncTaskLoader;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -32,36 +20,41 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton refreshButton;
     private TextView weather_info;
     private String weather_text_info="";
+    //Weather data
     private String city = "Barcelona";
-    private String apiId = "6e36078b02f257df49f83be2314b0b32";
+    private final String apiId = "6e36078b02f257df49f83be2314b0b32";
+    private String currentTemp = "";
+    private String skyState = "";
+    private String tempMin = "";
+    private String tempMax = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getRequestButton = (Button) findViewById(R.id.get_weather);
-        weather_info = (TextView) findViewById(R.id.textView);
-        refreshButton = (ImageButton) findViewById(R.id.imageButton);
+        getRequestButton = findViewById(R.id.get_weather);
+        weather_info = findViewById(R.id.textView);
+        refreshButton = findViewById(R.id.imageButton);
 
         new weatherBackgroundTask().execute();
     }
 
-    static class weatherBackgroundTask extends AsyncTask<String, Void, String> {
-        /*@Override
+    class weatherBackgroundTask extends AsyncTask<String, Void, String> {
+        @Override
         protected void onPreExecute() {
            super.onPreExecute();
 
-            // Showing the ProgressBar, Making the main design GONE
+            /* Showing the ProgressBar, Making the main design GONE
             findViewById(R.id.loader).setVisibility(View.VISIBLE);
             findViewById(R.id.mainContainer).setVisibility(View.GONE);
-            findViewById(R.id.errorText).setVisibility(View.GONE);
-        }*/
+            findViewById(R.id.errorText).setVisibility(View.GONE);*/
+        }
 
         protected String doInBackground(String... args) {
-            String response = "";
+            String response;
             try {
-                response = getWeather("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apiId);
+                response = getRequest.getWeather("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apiId);
             } catch (IOException e) {
                 response = e.toString();
             }
@@ -70,66 +63,27 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String result) {
-
-
             try {
-                JSONObject jsonObj = new JSONObject(result);
-                JSONObject main = jsonObj.getJSONObject("main");
-                JSONObject sys = jsonObj.getJSONObject("sys");
-                JSONObject wind = jsonObj.getJSONObject("wind");
-                JSONObject weather = jsonObj.getJSONArray("weather").getJSONObject(0);
-
-                Long updatedAt = jsonObj.getLong("dt");
-                String updatedAtText = "Updated at: " + new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(new Date(updatedAt * 1000));
-                String temp = main.getString("temp") + "°C";
-                String tempMin = "Min Temp: " + main.getString("temp_min") + "°C";
-                String tempMax = "Max Temp: " + main.getString("temp_max") + "°C";
-                String pressure = main.getString("pressure");
-                String humidity = main.getString("humidity");
-
-                Long sunrise = sys.getLong("sunrise");
-                Long sunset = sys.getLong("sunset");
-                String windSpeed = wind.getString("speed");
-                String weatherDescription = weather.getString("description");
-
-                String address = jsonObj.getString("name") + ", " + sys.getString("country");
-
-                weather_info.setText(temp+" " +tempMin+" "+tempMax);
-
-                /* Populating extracted data into our views
-                addressTxt.setText(address);
-                updated_atTxt.setText(updatedAtText);
-                statusTxt.setText(weatherDescription.toUpperCase());
-                tempTxt.setText(temp);
-                temp_minTxt.setText(tempMin);
-                temp_maxTxt.setText(tempMax);
-                sunriseTxt.setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(sunrise * 1000)));
-                sunsetTxt.setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(sunset * 1000)));
-                windTxt.setText(windSpeed);
-                pressureTxt.setText(pressure);
-                humidityTxt.setText(humidity);*/
-
-                /* Views populated, Hiding the loader, Showing the main design
-                findViewById(R.id.loader).setVisibility(View.GONE);
-                findViewById(R.id.mainContainer).setVisibility(View.VISIBLE);
-                */
+                JSONtoVARS(result);
+                weather_info.setText(currentTemp+tempMin+tempMax+skyState);
 
                 //Refresh the weather data with a button
                 //using thread because internet request
                 //have to be done in background
                 refreshButton.setOnClickListener(view -> {
-                    //We have to do a thread for a network petition for work in background(mandatory)
+                    //We have to do a thread for a network petition to work in background(mandatory)
                     Thread thread = new Thread(() -> {
                         try {
-                            weather_text_info=getWeather("https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=6e36078b02f257df49f83be2314b0b32");
-                        } catch (IOException e) {
+                            weather_text_info=getRequest.getWeather("https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=6e36078b02f257df49f83be2314b0b32");
+                            JSONtoVARS(weather_text_info);
+                        } catch (Exception e) {
                             weather_text_info = e.toString();
                         }
                     });
                     thread.start();
                     try {
                         thread.join();
-                        weather_info.setText(weather_text_info);
+                        weather_info.setText(currentTemp+tempMin+tempMax+skyState);
                     } catch (InterruptedException e) {
                         weather_info.setText(e.toString());
                     }
@@ -144,17 +98,17 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    protected void JSONtoVARS(String jsonString) throws JSONException {
+        JSONObject jsonObj = new JSONObject(jsonString);
+        JSONObject main = jsonObj.getJSONObject("main");
+        JSONObject weather = jsonObj.getJSONArray("weather").getJSONObject(0);
 
-    protected String getWeather (String urlPetition) throws IOException{
-        StringBuilder result = new StringBuilder();
-        URL url = new URL(urlPetition);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        for (String line; (line = reader.readLine()) != null; ) {
-            result.append(line);
-        }
-        return result.toString();
+        currentTemp = main.getString("temp") + "°C";
+        tempMin = main.getString("temp_min") + "°C";
+        tempMax = main.getString("temp_max") + "°C";
+
+        skyState = weather.getString("description");
+
+        city = jsonObj.getString("name");
     }
-
 }
