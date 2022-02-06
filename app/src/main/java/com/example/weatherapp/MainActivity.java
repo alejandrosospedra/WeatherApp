5552b55916ca27665345429451fd8664363023af
alejandro.sospedra <alejandro.sospedra@gmail.com>
2022-02-06 01:51:21 +0100

    Add a method to change the text view

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.weatherapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
@@ -13,13 +16,34 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button getRequestButton;
+    private TextView weather_info;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getRequestButton = (Button) findViewById(R.id.get_weather);
+        weather_info = (TextView) findViewById(R.id.textView);
+
+        getRequestButton.setOnClickListener(new View.OnClickListener(){
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                /*String getRequest = "";
+                try {
+                    getRequest = getWeather("https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=6e36078b02f257df49f83be2314b0b32");
+                    weather_info.setText(getRequest);
+                } catch (IOException e) {
+                    weather_info.setText(getRequest);
+                }*/
+                weather_info.setText("Test");
+            }
+        });
     }
 
-    public String getWeather (String urlPetition) throws IOException{
+    protected String getWeather (String urlPetition) throws IOException{
         StringBuilder result = new StringBuilder();
         URL url = new URL(urlPetition);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -30,5 +54,4 @@ public class MainActivity extends AppCompatActivity {
         }
         return result.toString();
     }
-
 }
